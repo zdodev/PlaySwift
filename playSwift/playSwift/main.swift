87915d42860844b724a2278217ffdb680a966e86
zdodev <iver0945@gmail.com>
2020-11-14 17:08:11 +0900

    Automatic Reference Counting

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -1,1 +1,25 @@
+/// Automatic Reference Counting
 
+/// How ARC Works
+
+/// ARC in Action
+
+/// Strong Reference Cycles between Class Instances
+
+/// Resolving Strong Reference Cycles between Class Instances
+
+/// Weak References
+
+/// Unowned References
+
+/// Unowned Optional References
+
+/// Unowned References and Implicitly Unwrapped Optional Properties
+
+/// Strong Reference Cycles for Closures
+
+/// Resolving Strong Reference Cycles for Closures
+
+/// Defining a Capture List
+
+/// Weak and Unowned References
