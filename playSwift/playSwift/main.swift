e97cf3ce4c3ce2d366f8e9aad027438b2dd19bc7
zdodev <iver0945@gmail.com>
2020-11-16 00:03:06 +0900

    Characteristics of Memory Access

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -1,1 +1,26 @@
+/// Memory Safety
 
+/// Understanding Conflicting Access to Memory
+
+func understandingConflictingAccessToMemory() {
+    let one = 1
+    print("We're number \(one)!")
+}
+
+/// Characteristics of Memory Access
+
+func characteristicsOfMemoryAccess() {
+    func oneMore(than number: Int) -> Int {
+        number + 1
+    }
+    
+    var myNumber = 1
+    myNumber = oneMore(than: myNumber)
+    print(myNumber)
+}
+
+/// Conflicting Access to In-Out Parameters
+
+/// Conflicting Access to self in Methods
+
+/// Conflicting Access to Properties
