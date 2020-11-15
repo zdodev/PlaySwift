94b1757fb6b07bf6576d4f134795593ebfe85b56
zdodev <iver0945@gmail.com>
2020-11-15 22:11:44 +0900

    Strong Reference Cycles for Closures

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -208,9 +208,43 @@ func unownedReferencesAndImplicitlyUnwrappedOptionalProperties() {
     print("\(country.name)'s capital city is called \(country.capitalCity.name)")
 }
 
-unownedReferencesAndImplicitlyUnwrappedOptionalProperties()
-
 /// Strong Reference Cycles for Closures
+
+func strongReferenceCyclesForClosures() {
+    class HTMLElement {
+        let name: String
+        let text: String?
+        
+        lazy var asHTML: () -> String = {
+            if let text = self.text {
+                return "<\(self.name)>\(text)</\(self.name)>"
+            } else {
+                return "<\(self.name)/>"
+            }
+        }
+        
+        init(name: String, text: String? = nil) {
+            self.name = name
+            self.text = text
+        }
+        
+        deinit {
+            print("\(name) is being deinitialized")
+        }
+    }
+    
+    let heading = HTMLElement(name: "h1")
+    let defaultText = "some default text"
+    heading.asHTML = {
+        "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+    }
+    print(heading.asHTML())
+    
+    var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+    print(paragraph!.asHTML())
+    
+    paragraph = nil
+}
 
 /// Resolving Strong Reference Cycles for Closures
 
