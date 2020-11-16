0048bf6b37fff3bc9319a755d36c1940fb667a73
zdodev <iver0945@gmail.com>
2020-11-16 10:06:20 +0900

    Conflicting Access to In-Out Parameters

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -21,6 +21,35 @@ func characteristicsOfMemoryAccess() {
 
 /// Conflicting Access to In-Out Parameters
 
+func conflictingAccessToInOutParameters() {
+    var stepSize = 1
+    
+    func increment(_ number: inout Int) {
+        number += stepSize
+    }
+    
+//    increment(&stepSize)
+    
+    var copyOfStepSize = stepSize
+    increment(&copyOfStepSize)
+    
+    stepSize = copyOfStepSize
+    
+    func balance(_ x: inout Int, _ y: inout Int) {
+        let sum = x + y
+        x = sum / 2
+        y = sum - x
+    }
+    
+    var playerOneScore = 43
+    var playerTwoScore = 30
+    
+    balance(&playerOneScore, &playerTwoScore)
+//    balance(&playerOneScore, &playerOneScore)
+}
+
+conflictingAccessToInOutParameters()
+
 /// Conflicting Access to self in Methods
 
 /// Conflicting Access to Properties
