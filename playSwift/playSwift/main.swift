4bcf715b4e98f07462ff27a543f8a30bae346995
zdodev <iver0945@gmail.com>
2020-11-16 12:23:46 +0900

    Access Control

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -1,1 +1,55 @@
+/// Access Control
 
+/// Modules and Source Files
+
+/// Access Levels
+
+/// Guiding Principle of Access Levels
+
+/// Default Access Levels
+
+/// Access Levels for Single-Target Apps
+
+/// Access Levels for Frameworks
+
+/// Access Levels for Unit Test Targets
+
+/// Access Control Syntax
+
+/// Custom Types
+
+/// Tuple Types
+
+/// Function Types
+
+/// Enumeration Types
+
+/// Raw Values and Associated Values
+
+/// Nested Types
+
+/// Subclassing
+
+/// Constants, Variables, Properties, and Subscripts
+
+/// Getters and Setters
+
+/// Initializers
+
+/// Default Initializers
+
+/// Default Memberwise Initializers for Structure Types
+
+/// Protocols
+
+/// Protocol Inheritance
+
+/// Protocol Conformance
+
+/// Extensions
+
+/// Private Members in Extensions
+
+/// Generics
+
+/// Type Aliases
