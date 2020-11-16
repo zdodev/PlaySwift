933a7aa27b224ef4978ad48a0ec32ea8f5aa30d8
zdodev <iver0945@gmail.com>
2020-11-16 10:31:26 +0900

    Conflicting Access to Properties

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -52,4 +52,62 @@ conflictingAccessToInOutParameters()
 
 /// Conflicting Access to self in Methods
 
+func conflictingAccessToSelfInMethods() {
+    struct Player {
+        var name: String
+        var health: Int
+        var energy: Int
+        
+        static let maxHealth = 10
+        
+        mutating func restoreHealth() {
+            health = Player.maxHealth
+        }
+        
+        func balance(_ x: inout Int, _ y: inout Int) {
+            let sum = x + y
+            x = sum / 2
+            y = sum - x
+        }
+        
+        mutating func shareHealth(with teammate: inout Player) {
+            balance(&teammate.health, &health)
+        }
+    }
+    
+    var oscar = Player(name: "Oscar", health: 10, energy: 10)
+    var maria = Player(name: "Maria", health: 5, energy: 10)
+    oscar.shareHealth(with: &maria)
+    
+    // Error
+//    oscar.shareHealth(with: &oscar)
+}
+
 /// Conflicting Access to Properties
+
+func conflictingAccessToProperties() {
+    func balance(_ x: inout Int, _ y: inout Int) {
+        let sum = x + y
+        x = sum / 2
+        y = sum - x
+    }
+
+    var playerInformation = (health: 10, energy: 20)
+    // 에러가 출력되야 합니다..
+    balance(&playerInformation.health, &playerInformation.energy)
+
+    struct Player {
+        var name: String
+        var health: Int
+        var energy: Int
+    }
+
+    var holly = Player(name: "Holly", health: 10, energy: 10)
+    // 에러가 출력되야 합니다..
+    balance(&holly.health, &holly.energy)
+    
+    func someFunction() {
+        var oscar = Player(name: "Oscar", health: 10, energy: 10)
+        balance(&oscar.health, &oscar.energy)
+    }
+} 
