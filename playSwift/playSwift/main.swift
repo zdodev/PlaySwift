9f8fd94a3d775368f64f1c421470e285418fce65
zdodev <iver0945@gmail.com>
2020-11-16 13:57:14 +0900

    Custom Types

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -16,7 +16,42 @@
 
 /// Access Control Syntax
 
+open class SomeOpenClass {}
+public class SomePublicClass {}
+internal class SomeInternalClass {} // Defalut Access Level
+fileprivate class SomeFilePrivateClass {}
+private class SomePrivateClass {}
+
+public func somePublicFunction() {}
+internal func someInternalFunction() {} // Defalut Access Level
+fileprivate func someFilePrivateFunction() {}
+private func somePrivateFunction() {}
+
+public let someOpenConstant = 0
+internal let someInternalConstant = 0 // Defalut Access Level
+fileprivate let someFilePrivateConstant = 0
+private let somePrivateConstant = 0
+
 /// Custom Types
+
+public class SomePublicClass1 {
+    public var somePublicProperty = 0
+    var someInternalProperty = 0
+    fileprivate var someFilePrivateProperty = 0
+    private var somePrivateProperty = 0
+    
+    public func somePublicFunction() {}
+    func someInternalFunction() {}
+    fileprivate func someFilePrivateFunction() {}
+    private func somePrivateFunction() {}
+}
+
+class SomeInternalClass1 {
+    public var somePublicProperty = 0
+    var someInternalProperty = 0
+    fileprivate var someFilePrivateProperty = 0
+    private var somePrivateProperty = 0
+}
 
 /// Tuple Types
 
