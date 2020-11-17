dc95ea1a52d7c866dace6faa80689924a3062235
zdodev <iver0945@gmail.com>
2020-11-17 16:33:23 +0900

    Precedence for Custom Infix Operators

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -168,4 +168,34 @@ func equivalenceOperators() {
 
 /// Custom Operators
 
+prefix operator +++
+
+extension Vector2D {
+    static prefix func +++ (vector: inout Vector2D) -> Vector2D {
+        vector += vector
+        return vector
+    }
+}
+
+func customOperators() {
+    var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+    let afterDoubling = +++toBeDoubled
+    print(afterDoubling)
+}
+
 /// Precedence for Custom Infix Operators
+
+infix operator +-: AdditionPrecedence
+
+extension Vector2D {
+    static func +- (left: Vector2D, right: Vector2D) -> Vector2D {
+        Vector2D(x: left.x + right.x, y: left.y - right.y)
+    }
+}
+
+func precedenceForCustomInfixOperators() {
+    let firstVector = Vector2D(x: 1.0, y: 2.0)
+    let secondVector = Vector2D(x: 3.0, y: 4.0)
+    let plusMinusVector = firstVector +- secondVector
+    print(plusMinusVector)
+}
