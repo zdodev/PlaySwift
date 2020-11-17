349a35abafd29a69966df65459ee89f9e1df159e
zdodev <iver0945@gmail.com>
2020-11-17 15:38:24 +0900

    Precedence and Associativity

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -41,13 +41,52 @@ func bitwiseXorOperator() {
 
 /// Shifting Behavior for Unsigned Integers
 
+func shiftingBehaviorForUnsignedIntegers() {
+    let shiftBits: UInt8 = 0b100
+    _ = shiftBits << 1
+    _ = shiftBits << 2
+    _ = shiftBits << 5
+    _ = shiftBits << 6
+    _ = shiftBits << 2
+    
+    let pink: UInt32 = 0xCC6699
+    let redComponent = (pink & 0xFF0000) >> 16
+    let greenComponent = (pink & 0x00FF00) >> 8
+    let blueComponent = pink & 0x0000FF
+}
+
 /// Shifting Behavior for Signed Integers
 
 /// Overflow Operators
 
+func overflowOperators() {
+    var potentialOverflow = Int16.max
+//    potentialOverflow += 1
+}
+
 /// Value Overflow
 
+func valueOverflow() {
+    var unsignedOverflow = UInt8.max
+    unsignedOverflow = unsignedOverflow &+ 1
+    print(unsignedOverflow)
+    
+    unsignedOverflow = UInt8.min
+    unsignedOverflow = unsignedOverflow &- 1
+    print(unsignedOverflow)
+    
+    var signedOverflow = Int8.min
+    signedOverflow &-= 1
+    print(signedOverflow)
+}
+
+valueOverflow()
+
 /// Precedence and Associativity
+
+func precedenceAndAssociativity() {
+    print(2 + 3 % 4 * 5)
+}
 
 /// Operator Methods
 
