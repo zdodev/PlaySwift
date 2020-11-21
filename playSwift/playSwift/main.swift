7407efbc892c0ef2cae0bb8a66ce82b065dd140b
zdodev <iver0945@gmail.com>
2020-11-21 18:57:16 +0900

    Type Annotations

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -23,3 +23,19 @@ var age = 500
 var x = 0.0, y = 0.0, z = 0.0
 
 // Type Annotations
+
+// 상수 또는 변수를 선언할 때 타입을 명시하여 선언할 수 있습니다.
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+// 콤마(,)를 사용하여 한 줄에 같은 타입의 변수를 선언할 수 있습니다.
+
+var red, green, blue: Double
+
+/*
+ 실제로 type annotations를 사용하지 않더라도 Swift의 타입 추론을 통해 변수 선언 시 초기 값을 제공하면
+ Swift에서 타입을 추론하여 지정합니다.
+ */
+
+
