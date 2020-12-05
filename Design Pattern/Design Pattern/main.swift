eb7dc0f742ff7f82ba1254d32b6971307f612c90
zdodev <iver0945@gmail.com>
2020-12-05 20:40:58 +0900

    Design Pattern

--- Design Pattern/Design Pattern/main.swift
+++ Design Pattern/Design Pattern/main.swift
@@ -1,11 +1,76 @@
-//
-//  main.swift
-//  Design Pattern
-//
-//  Created by zdo on 2020/12/04.
-//
-
 import Foundation
 
-print("Hello, World!")
+enum A {
+    class ConsoleApplication: PlatformInterface {
+        var logic: BusinessLogic
+        
+        func getTime() {
+            logic.getCurrentTime()
+        }
+        
+        func updateTime(time: String) {
+            print(time)
+        }
+    }
+    
+    protocol PlatformInterface {
+        func updateTime(time: String)
+    }
+    
+    class BusinessLogic {
+        var platform: PlatformInterface
+        
+        func getCurrentTime() {
+            let time = "\(Date())"
+            platform.updateTime(time: time)
+        }
+    }
+}
 
+enum B {
+    class Repository: PlatformInterface {
+        func fetchData(_ callback: @escaping (Data) -> Void) {
+            URLSession.shared.dataTask(with: URL()) { (data, _, _) in
+                callback(data!)
+            }
+        }
+    }
+    
+    protocol PlatformInterface {
+        func fetchData(_ callback: @escaping (Data) -> Void)
+    }
+    
+    class BusinessLogic {
+        var platform: PlatformInterface
+        
+        func handleData() {
+            platform.fetchData { (Data) in
+                //
+            }
+        }
+    }
+}
+
+enum C {
+    class NetworkImpl: Network {
+        func GET(url: URL, callback: @escaping (Data) -> Void) {
+            URLSession.shared.dataTask(with: url) { (data, _, _) in
+                callback(data!)
+            }
+        }
+    }
+    
+    protocol Network {
+        func GET(url: URL, callback: @escaping (Data) -> Void)
+    }
+    
+    class BusinessLogic {
+        var platform = Network?
+        
+        func fetchData(urlString: String) {
+            platform.GET(url: URL(string: urlString!) { data in
+                //
+            }
+            }
+    }
+}
