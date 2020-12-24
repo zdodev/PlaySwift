26710147257a9882e24d83acce568a9992ed7048
zdodev <iver0945@gmail.com>
2020-12-25 01:50:38 +0900

    Multiple String Literals

--- playSwift/playSwift/main.swift
+++ playSwift/playSwift/main.swift
@@ -1,1 +1,42 @@
 /// Strings and Characters
+
+/// String Literals
+
+let someString = "Some string literal value"
+
+// Multiline String Literals
+
+let quotation = """
+The White Rabbit put on his spectacles. "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+
+let singleLineString = "These are the same."
+
+let multilineString = """
+These are the same.
+"""
+
+let softWrappedQuotation = """
+The White Rabbit put on his spectacles. "Where shall I begin, \
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop."
+"""
+
+let lineBreaks = """
+
+This string starts with a line break.
+It also ends with a line break.
+
+"""
+
+let linesWithIndentation = """
+    This line doesn't begin with whitespace.
+        This line begins with four spaces.
+    This line doesn't begin with whitespace.
+    """
