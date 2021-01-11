10ac7254d51a668ac36ea78a8609430c8f782304
zdodev <iver0945@gmail.com>
2021-01-11 09:38:56 +0900

    😎: Solve the LeetCode 125. Valid Palindrome problem.

---
+++ Algorithm/LeetCode125ValidPalindrome/LeetCode125ValidPalindrome/main.swift
@@ -0,0 +1,48 @@
+//
+//  main.swift
+//  LeetCode125ValidPalindrome
+//
+//  Created by Zero DotOne on 2021/01/11.
+//
+
+/*
+ 125. Valid Palindrome
+ 
+ Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ 
+ Note: For the purpose of this problem, we define empty string as valid palindrome.
+ */
+
+import Foundation
+
+protocol Solving {
+    func isPalindrome(_ s: String) -> Bool
+}
+
+class Solution: Solving {
+    func isPalindrome(_ s: String) -> Bool {
+        let lowercasedString = s.lowercased()
+        let newString = extractLettersAndNumbers(lowercasedString)
+        
+        guard !newString.isEmpty else {
+            return true
+        }
+        
+        var (leftIndex, rightIndex) = (newString.startIndex, newString.index(before: newString.endIndex))
+        while leftIndex < rightIndex {
+            if newString[leftIndex] != newString[rightIndex] {
+                return false
+            }
+            leftIndex = newString.index(after: leftIndex)
+            rightIndex = newString.index(before: rightIndex)
+        }
+
+        return true
+    }
+    
+    private func extractLettersAndNumbers(_ lowercasedString: String) -> String {
+        return lowercasedString.filter {
+            $0.isLetter || $0.isNumber
+        }
+    }
+}
