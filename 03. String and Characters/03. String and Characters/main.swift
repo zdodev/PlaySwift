09b0228efd3c2cc37fbe3f6ba77e4384e5b9cff7
zdodev <iver0945@gmail.com>
2021-01-16 16:15:41 +0900

    ✨: String.

--- 03. String and Characters/03. String and Characters/main.swift
+++ 03. String and Characters/03. String and Characters/main.swift
@@ -1,11 +1,31 @@
-//
-//  main.swift
-//  03. String and Characters
-//
-//  Created by Zero DotOne on 2021/01/16.
-//
+/// String Literals
 
-import Foundation
+let someString = "Some string literal value"
 
-print("Hello, World!")
+/// Multiline String Literals
 
+let quotation = """
+The White Rabbit put on his spetacles. "Where shall I begin,
+please your Majesty? ha asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+
+let singleLineString = "There are the same."
+let multilineString = """
+There are the same.
+"""
+
+let softWrappedQuotation = """
+The White Rabbit put on his spetacles. "Where shall I begin, \
+please your Majesty? ha asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop."
+"""
+
+/// Special Characters in String Literals
+
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+let dollarSign = "\u{24}"
