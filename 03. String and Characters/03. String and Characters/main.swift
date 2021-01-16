c54413efcfe7c7968c6d5ba4e6ee067697d47287
zdodev <iver0945@gmail.com>
2021-01-16 16:01:31 +0900

    🎉: Begin a project.

---
+++ 03. String and Characters/03. String and Characters/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  03. String and Characters
+//
+//  Created by Zero DotOne on 2021/01/16.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- Operation/Operation/main.swift
+++ Operation/Operation/main.swift
@@ -8,47 +8,56 @@
 import Foundation
 
 class Calculator: Operation {
+    var a = 0
+    
     /// non-concurrent task
     override func main() {
         for index in 1...5 {
             print("\(index)...🌸")
         }
+        a += 1
+        print(a)
     }
 }
+let opq = OperationQueue()
+let a = Calculator()
+opq.addOperation(a)
+opq.waitUntilAllOperationsAreFinished()
 
-class Calculator1: Operation {
-    /// non-concurrent task
-    override func main() {
-        for index in 1...5 {
-            print("\(index)...🌷")
+
+//var balance = 1200
+
+struct ATM {
+    var balance = 1200
+    
+    let tag: String
+    mutating func withdraw(value: Int) {
+        print("\(self.tag): checking if balance containing sufficent money")
+        if balance > value {
+            print("\(self.tag): Balance is sufficent, please wait while processing withdrawal")
+            // sleeping for some random time, simulating a long process
+            Thread.sleep(forTimeInterval: Double.random(in: 0...2))
+            balance -= value
+            print("\(self.tag): Done: \(value) has been withdrawed")
+            print("\(self.tag): current balance is \(balance)")
+        } else {
+            print("\(self.tag): Can't withdraw: insufficent balance")
         }
     }
 }
 
-class Calculator2: Operation {
-    /// non-concurrent task
-    override func main() {
-        for index in 1...5 {
-            print("\(index)...🌺")
-        }
-    }
+let queue = DispatchQueue(label: "WithdrawalQueue", attributes: .concurrent)
+
+queue.async {
+    var firstATM = ATM(tag: "firstATM")
+    firstATM.withdraw(value: 1000)
 }
 
-let operationQueue = OperationQueue()
+queue.async {
+    var secondATM = ATM(tag: "secondATM")
+    secondATM.withdraw(value: 800)
+}
+Thread.sleep(forTimeInterval: 5)
+//print()
 
-let calculator1 = Calculator()
-let calculator2 = Calculator1()
-let calculator3 = Calculator2()
 
-calculator1.addDependency(calculator2)
-calculator3.addDependency(calculator1)
-
-operationQueue.addOperation(calculator1)
-operationQueue.addOperation(calculator2)
-operationQueue.addOperation(calculator3)
-
-//operationQueue.addOperation {
-//    print("끝")
-//}
-
-operationQueue.waitUntilAllOperationsAreFinished()
