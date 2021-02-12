a455c7025aa934ae5d39b4ada65ae9d080605a88
zdodev <iver0945@gmail.com>
2021-02-13 07:47:59 +0900

    ✨: Loop.

--- Swift-Language/05. Control Flow/Control Flow/main.swift
+++ Swift-Language/05. Control Flow/Control Flow/main.swift
@@ -39,3 +39,71 @@ let hourInterval = 3
 for tickMark in stride(from: 3, through: hours, by: hourInterval) {
 //    print("tickMark = \(tickMark)")
 }
+
+let finalSquare = 25
+var board = [Int](repeating: 0, count: finalSquare + 1)
+
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+var square = 0
+var diceRoll = 0
+while square < finalSquare {
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    square += diceRoll
+    if square < board.count {
+        square += board[square]
+    }
+}
+print("Game over!")
+
+/// Repeat-While
+
+square = 0
+diceRoll = 0
+repeat {
+    square += board[square]
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    square += diceRoll
+} while square < finalSquare
+
+/// Conditional Statements
+
+/// If
+
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+}
+
+temperatureInFahrenheit = 40
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+
+temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+
+temperatureInFahrenheit = 72
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+}
+// No message is printed.
+
+/// Switch
