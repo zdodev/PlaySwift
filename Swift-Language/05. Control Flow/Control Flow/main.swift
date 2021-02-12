06c2a3980612f8f78af46bfb4abe61d034f6555b
zdodev <iver0945@gmail.com>
2021-02-13 05:05:50 +0900

    For-In Loops.

--- Swift-Language/05. Control Flow/Control Flow/main.swift
+++ Swift-Language/05. Control Flow/Control Flow/main.swift
@@ -1,11 +1,41 @@
-//
-//  main.swift
-//  Control Flow
-//
-//  Created by Zero DotOne on 2021/02/13.
-//
+/// Control Flow
 
-import Foundation
+/// For-In Loops
 
-print("Hello, World!")
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names {
+    print("Hello, \(name)!")
+}
 
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs.")
+}
+
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+    answer *= base
+}
+print("\(base) to the power of \(power) is \(answer)")
+
+let minutes = 60
+for tickMark in 0..<minutes {
+//    print("tickMark = \(tickMark)")
+}
+
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval) {
+//    print("tickMark = \(tickMark)")
+}
+
+let hours = 12
+let hourInterval = 3
+for tickMark in stride(from: 3, through: hours, by: hourInterval) {
+//    print("tickMark = \(tickMark)")
+}
