98f27b16c429263f4f39429ffc134b17deed72ce
zdodev <iver0945@gmail.com>
2021-02-13 18:35:30 +0900

    Checking API availability.

--- Swift-Language/05. Control Flow/Control Flow/main.swift
+++ Swift-Language/05. Control Flow/Control Flow/main.swift
@@ -107,3 +107,236 @@ if temperatureInFahrenheit <= 32 {
 // No message is printed.
 
 /// Switch
+
+let someCharacter: Character = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+
+/// No Implicit Fallthrough
+
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+//case "a":
+case "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+
+switch anotherCharacter {
+case "a", "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+
+/// Interval Matching
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+/// Tuples
+
+let somePoint = (1, 1)
+switch somePoint {
+case (0, 0):
+    print("\(somePoint) is at the origin")
+case (_, 0):
+    print("\(somePoint) is on the x-axis")
+case (0, _):
+    print("\(somePoint) is on the y-axis")
+case (-2...2, -2...2):
+    print("\(somePoint) is inside the box")
+default:
+    print("\(somePoint) is outside of the box")
+}
+
+/// Value Binding
+
+let anotherPoint = (2, 0)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x value of \(x)")
+case (0, let y) :
+    print("on the y-axis with an y value of \(y)")
+case let (x, y):
+    print("somewhere else at (\(x), \(y)")
+}
+
+/// Where
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+
+/// Compound Cases
+
+let someCharacter1: Character = "e"
+switch someCharacter1 {
+case "a", "e", "i", "o", "u":
+    print("\(someCharacter1) is a vowel")
+case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+     "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+    print("\(someCharacter1) is a consonant")
+default:
+    print("\(someCharacter1) is not a vowel or a consonant")
+}
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
+
+/// Control Transfer Statements
+/*
+ - continue
+ - break
+ - fallthrough
+ - return
+ - throw
+ */
+
+/// Continue
+
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+let charactersToRemove: [Character] = ["a", "e", "i", "o", "u", " "]
+for character in puzzleInput {
+    if charactersToRemove.contains(character) {
+        continue
+    }
+    puzzleOutput.append(character)
+}
+print("puzzleOutput = \(puzzleOutput)")
+
+/// Break
+
+/// Break in a Loop Statement
+
+/// Break in a Switch Statement
+
+let numberSymbol: Character = "三"
+var possibleIntegerValue: Int?
+switch numberSymbol {
+case "1", "١", "一", "๑":
+    possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+    possibleIntegerValue = 2
+case "3", "٣", "三", "๓":
+    possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+    possibleIntegerValue = 4
+default:
+    break
+}
+if let integerValue = possibleIntegerValue {
+    print("The integer value of \(numberSymbol) is \(integerValue)")
+} else {
+    print("An integer value could not be found for \(numberSymbol)")
+}
+
+/// Fallthrough
+
+let integerToDescribe = 5
+var description = "The nubmer \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also"
+    fallthrough
+default:
+    description += " an integer."
+}
+print("description = \(description)")
+
+/// Labeled Statements
+
+square = 0
+diceRoll = 0
+
+gameLoop: while square != finalSquare {
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    switch square + diceRoll {
+    case finalSquare:
+        break gameLoop
+    case let newSquare where newSquare > finalSquare:
+        continue gameLoop
+    default:
+        square += diceRoll
+        square += board[square]
+    }
+}
+print("Game over!")
+
+/// Early Exit
+/*
+ - return
+ - break
+ - continue
+ - throw
+ */
+
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    
+    print("Hello \(name)!")
+    
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    
+    print("I hope the weather is nice in \(location).")
+}
+
+greet(person: ["name": "John"])
+greet(person: ["name": "Jane", "location": "Cupertino"])
+
+/// Checking API Availabilty
+
+if #available(iOS 10, macOS 10.12, *) {
+    print("Use iOS 10 APIs on iOS, and use macOS 10.12 APIs on macOS.")
+} else {
+    print("Fall back to earlier iOS and macOS APIs.")
+}
+
+if #available(iOS 14.2, *) {
+    print("Use iOS 14.2 APIs on iOS.")
+} else {
+    print("fall back to earlier iOS APIs.")
+}
