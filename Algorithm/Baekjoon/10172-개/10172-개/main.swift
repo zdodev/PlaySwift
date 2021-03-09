f98d04ac42d0eb4bb1d1bcfd08c58bbefa92cd89
zdodev <iver0945@gmail.com>
2021-03-09 21:48:21 +0900

    🎯: Beakjoon-10172.

---
+++ Algorithm/Baekjoon/10172-개/10172-개/main.swift
@@ -0,0 +1,19 @@
+final class Solution {
+    func result1() {
+        print(#"|\_/|"#)
+        print(#"|q p|   /}"#)
+        print(#"( 0 )"""\"#)
+        print(#"|"^"`    |"#)
+        print(#"||_/=\\__|"#)
+    }
+    
+    func result2() {
+        print("|\\_/|")
+        print("|q p|   /}")
+        print("( 0 )\"\"\"\\")
+        print("|\"^\"`    |")
+        print("||_/=\\\\__|")
+    }
+}
+
+Solution().result1()
---
+++ Algorithm/Baekjoon/README.md
@@ -0,0 +1,15 @@
+### Baekjoon-10172번 개
+
+- 구현 문제
+
+### Baekjoon-10171번 고양이
+
+- 구현 문제
+
+### Baekjoon-10718번 We love kriii
+
+- 구현 문제
+
+### Baekjoon-2557번 Hello World
+
+- 구현 문제
