7b6e72c0a68667bf2c4cb9b15156a7262f718f81
zdodev <iver0945@gmail.com>
2021-03-09 20:00:51 +0900

    🎯: Beakjoon-10171.

---
+++ Algorithm/Baekjoon/10171-고양이/10171-고양이/main.swift
@@ -0,0 +1,17 @@
+final class Solution {
+    func result1() {
+        print(#"\    /\"#)
+        print(#" )  ( ')"#)
+        print(#"(  /  )"#)
+        print(#" \(__)|"#)
+    }
+    
+    func result2() {
+        print("\\    /\\")
+        print(" )  ( ')")
+        print("(  /  )")
+        print(" \\(__)|")
+    }
+}
+
+Solution().result2()
