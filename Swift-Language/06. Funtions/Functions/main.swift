cd472cdee9b83cb68d057d4e8e79de4a7388eae2
zdodev <iver0945@gmail.com>
2021-03-23 02:18:54 +0900

    🙇‍♂️: Functions with multiple parameters.

--- Swift-Language/06. Funtions/Functions/main.swift
+++ Swift-Language/06. Funtions/Functions/main.swift
@@ -1,4 +1,4 @@
-/// Defining and Calling Functions
+// MARK: Defining and Calling Functions
 
 func greet(person: String) -> String {
     let greeting = "Hello, " + person + "!"
@@ -11,3 +11,23 @@ func greetAgain(person: String) -> String {
     return "Hello again, " + person + "!"
 }
 print(greetAgain(person: "Anna"))
+
+// MARK: - Function Parameters and Return Values
+
+// MARK: Functions without Parameters
+
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+
+// MARK: Functions with Multiple Paramters
+
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Tim", alreadyGreeted: true))
