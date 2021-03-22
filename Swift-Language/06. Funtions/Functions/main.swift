612da3015145ec51abfff7943664e10cc0b0501a
zdodev <iver0945@gmail.com>
2021-03-23 02:48:02 +0900

    🙇‍♂️: Functions with multiple return values.

--- Swift-Language/06. Funtions/Functions/main.swift
+++ Swift-Language/06. Funtions/Functions/main.swift
@@ -50,3 +50,22 @@ func printWithoutCounting(string: String) {
 
 print(printAndCount(string: "hello, world"))
 printWithoutCounting(string: "hello, world")
+
+// MARK: Functions with multiple return values
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+
