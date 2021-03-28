6700872f14f273a20f1945e25a637c6f4617a26b
zdodev <iver0945@gmail.com>
2021-03-28 15:47:18 +0900

    📖: Closure expressions.

--- Swift-Language/07. Closures/Closures/main.swift
+++ Swift-Language/07. Closures/Closures/main.swift
@@ -1,4 +1,8 @@
-import Foundation
+// MARK: - Closures
+
+// MARK: - Closure Expressions
+
+// MARK: The Sorted Method
 
 let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
 
@@ -9,27 +13,62 @@ func backward(_ s1: String, _ s2: String) -> Bool {
 var reversedNames = names.sorted(by: backward)
 print(reversedNames)
 
-// 완전한 클로저 표현식
+// MARK: Closure Expression Syntax
+
+// sorted(by:) 메서드의 파라미터 타입: (String, String) -> Bool
+
+/*
+ 클로저 정의
+ { (s1: String, s2: String) -> Bool in
+     return s1 > s2
+ }
+ */
 reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
     return s1 > s2
 })
 
-// 타입 추론으로 타입을 생략 클로저
+
+// MARK: Inferring Type from Context
+
+// sorted(by:) 파라미터는 (String, String) -> Bool 타입을 받으므로 클로저에서 파라미터 타입과 리턴 타입을 생략해도 타입을 추론할 수 있다.
+
+/*
+ 파라미터 타입과 리턴 타입을 생략한 클로저
+ { s1, s2 in
+     return s1 > s2
+ }
+ */
 reversedNames = names.sorted(by: { s1, s2 in
     return s1 > s2
 })
 
-// 단일 표현식으로 return 키워드 생략 클로저
+// MARK: Implicit Returns from Single-Expression Closures
+
 reversedNames = names.sorted(by: { s1, s2 in
     s1 > s2
 })
 
-// 단축 인자 표현으로 인자명 생략 클로저
+// MARK: Shorthand Argument Names
+
+// sorted(by:) 파라미터는 (String, String) -> Bool 타입을 받으므로 인자명을 정의하지 않고 $0, $1와 같이 단축 인자명을 사용할 수 있으며 in 키워드를 생략할 수 있다.
+
+/*
+ 파라미터명과 in 키워드를 생략한 클로저
+ {
+     $0 > $1
+ }
+ */
+
 reversedNames = names.sorted(by: {
     $0 > $1
 })
 
-// 연산자 메서드 클로저
+// MARK: Operator Methods
+
+// sorted(by:) 파라미터는 (String, String) -> Bool 타입을 받는다. 그리고 (String, String) -> Bool 이 구현된 연산자가 있을 경우 연산자만을 사용하여 클로저를 표현할 수 있다.
+
+// static func > (lhs: String, rhs: String) -> Bool
+
 reversedNames = names.sorted(by: >)
 
 // 후행 클로저
