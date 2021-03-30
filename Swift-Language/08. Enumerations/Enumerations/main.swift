6ac2990451bdefbd2df300ca8c3f24086b53397a
zdodev <iver0945@gmail.com>
2021-03-30 13:14:54 +0900

    🎉: Enumerations.

---
+++ Swift-Language/08. Enumerations/Enumerations/main.swift
@@ -0,0 +1,15 @@
+// MARK: Enumeration Syntax
+
+// MARK: - Matching Enumeration Values with a Switch Statement
+
+// MARK: - Iterating over Enumeration Cases
+
+// MARK: - Associated Values
+
+// MARK: - Raw Values
+
+// MARK: Implicitly Assigned Raw Values
+
+// MARK: Initializing from a Raw Value
+
+// MARK: - Recursive Enumerations
