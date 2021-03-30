39d64a8d58628ec0ce8744b3368b0a8d6a41094e
zdodev <iver0945@gmail.com>
2021-03-31 01:19:29 +0900

    🎉: Begin methods project.

---
+++ Swift-Language/11. Methods/Methods/main.swift
@@ -0,0 +1,11 @@
+// MARK: - Methods
+
+// MARK: - Instance Methods
+
+// MARK: The self Property
+
+// MARK: Modifying Value Types from within Instance Methods
+
+// MARK: Assigning to self within a Mutating Method
+
+// MARK: - Type Methods
