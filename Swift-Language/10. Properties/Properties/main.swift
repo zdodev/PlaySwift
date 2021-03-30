3034e7d4b7268d007432b4794da8acde0596677e
zdodev <iver0945@gmail.com>
2021-03-31 01:12:08 +0900

    🎉: Begin properties project.

---
+++ Swift-Language/10. Properties/Properties/main.swift
@@ -0,0 +1,33 @@
+// MARK: - Properties
+
+// MARK: - Stored Properties
+
+// MARK: Stored Properties of Constant Structure Instances
+
+// MARK: Lazy Stored Properties
+
+// MARK: Stored Properties and Instance Variables
+
+// MARK: - Computed Properties
+
+// MARK: Shorthand Setter Declaration
+
+// MARK: Shorthand Getter Declaration
+
+// MARK: Read-Only Computed Properties
+
+// MARK: - Property Observers
+
+// MARK: - Property Wrappers
+
+// MARK: Setting Initial Values for Wrapped Properties
+
+// MARK: Projecting a Value from a Property Wrapper
+
+// MARK: - Global and Local Variables
+
+// MARK: - Type Properties
+
+// MARK: Type Property Syntax
+
+// MARK: Querying and Setting Type Properties
