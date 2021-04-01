0e1ef92b71c69ee1a30d64e4c24eaedb3db4fd7e
zdodev <iver0945@gmail.com>
2021-04-01 13:50:57 +0900

    🎉: Begin a type casting project.

---
+++ Swift-Language/18. Type Casting/Type Casting/main.swift
@@ -0,0 +1,10 @@
+// MARK: - Type Casting
+
+// MARK: - Defining a Class Hierarchy for Type Casting
+
+// MARK: - Checking Type
+
+// MARK: - Downcasting
+
+// MARK: - Type Casting for Any and AnyObject
+
