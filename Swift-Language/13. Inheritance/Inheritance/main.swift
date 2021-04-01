9e53ab072d1758e59383e25070a100d7bd3147b6
zdodev <iver0945@gmail.com>
2021-04-01 13:00:34 +0900

    🎉: Begin a inheritance project.

---
+++ Swift-Language/13. Inheritance/Inheritance/main.swift
@@ -0,0 +1,19 @@
+// MARK: - Inheritance
+
+// MARK: - Defining a Base Class
+
+// MARK: - Subclassing
+
+// MARK: - Overriding
+
+// MARK: Accessing Superclass Methods, Properties, and Subscripts
+
+// MARK: Overriding Methods
+
+// MARK: Overriding Properties
+
+// MARK: Overriding Property Getters and Setters
+
+// MARK: Overriding Property Observers
+
+// MARK: - Preventing Overrides
