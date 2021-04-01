6b1c6e93185660660c68d10b6d5c64c2238092e0
zdodev <iver0945@gmail.com>
2021-04-01 13:46:57 +0900

    🎉: Begin a error handling project.

---
+++ Swift-Language/17. Error Handling/Error Handling/main.swift
@@ -0,0 +1,15 @@
+// MARK: - Error Handling
+
+// MARK: - Representing and Throwing Errors
+
+// MARK: - Handling Errors
+
+// MARK: Propagating Errors Using Throwing Functions
+
+// MARK: Handling Errors Using Do-Catch
+
+// MARK: Converting Errors to Optional Values
+
+// MARK: Disabling Error Propagation
+
+// MARK: - Specifying Cleanup Actions
