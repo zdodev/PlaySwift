f29a504ff46cf90ab08e9d21489d422055a3adb4
zdodev <iver0945@gmail.com>
2021-04-01 13:13:13 +0900

    🎉: Begin a initialization project.

---
+++ Swift-Language/14. Initialization/Initialization/main.swift
@@ -0,0 +1,55 @@
+// MARK: - Initialization
+
+// MARK: - Setting Initial Values for Stored Properties
+
+// MARK: Initializers
+
+// MARK: Default Property Values
+
+// MARK: - Customizing Initialization
+
+// MARK: Initialization Parameters
+
+// MARK: Parameter Names and Argument Labels
+
+// MARK: Initializer Parameters without Argument Labels
+
+// MARK: Optional Property Types
+
+// MARK: Assigning Constant Properties During Initialization
+
+// MARK: - Default Initializers
+
+// MARK: Memberwise Initializers for Structure Types
+
+// MARK: - Initializer Delegation for Value Types
+
+// MARK: - Class Inheritance and Initialization
+
+// MARK: Designated Initializers and Convenience Initializers
+
+// MARK: Syntax for Designated and Convenience Initializers
+
+// MARK: Initializer Delegation for Class Types
+
+// MARK: Two-Phase Initialization
+
+// MARK: Initializer Inheritance and Overriding
+
+// MARK: Automatic Initializer Inheritance
+
+// MARK: Designated and Convenience Initializers in Action
+
+// MARK: - Failable Initializers
+
+// MARK: Failable Initializers for Enumerations
+
+// MARK: Failable Initializers for Enumerations with Raw Values
+
+// MARK: Propagation of Initialization Failure
+
+// MARK: Overriding Failable Initializer
+
+// MARK: - Required Initializers
+
+// MARK: - Setting a Default Property Value with a Closure or Function
