d40fde489a772a57a40cc90b42b99471630084fe
zdodev <iver0945@gmail.com>
2021-04-01 13:37:38 +0900

    🎉: Begin a optional chaining project.

---
+++ Swift-Language/16. Optional Chaining/Optional Chaining/main.swift
@@ -0,0 +1,15 @@
+// MARK: - Optional Chaining
+
+// MARK: - Optional Chaining as an Alternative to Forced Unwrapping
+
+// MARK: - Defining Model Classes for Optional Chaining
+
+// MARK: - Accessing Properties thorugh Optional Chaining
+
+// MARK: - Calling Methods through Optional Chaining
+
+// MARK: - Accessing Subscripts through Optional Chaining
+
+// MARK: - Linking Multiple Levels of Chaining
+
+// MARK: - Chaining on Methods with Optional Return Values
