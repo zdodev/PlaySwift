f8f2a229119f640cc32f4571f32de3054872ff3c
zdodev <iver0945@gmail.com>
2021-04-06 13:28:03 +0900

    🧮: Baekjoon 5337번 웰컴.

---
+++ Algorithm/Baekjoon/5337-웰컴/5337-웰컴/main.swift
@@ -0,0 +1,9 @@
+struct Solution {
+    func solution() {
+        print(".  .   .")
+        print("|  | _ | _. _ ._ _  _")
+        print("|/\\|(/.|(_.(_)[ | )(/.")
+    }
+}
+
+Solution().solution()
