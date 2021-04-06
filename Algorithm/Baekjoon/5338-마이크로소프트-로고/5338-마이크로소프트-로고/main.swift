7cbf3cedf76996fe482fe576eba0b4ab339436a8
zdodev <iver0945@gmail.com>
2021-04-06 13:34:48 +0900

    🧮: Baekjoon 5338번 마이크로소프트 로고.

---
+++ Algorithm/Baekjoon/5338-마이크로소프트-로고/5338-마이크로소프트-로고/main.swift
@@ -0,0 +1,11 @@
+struct Solution {
+    func solution() {
+        print("       _.-;;-._")
+        print("'-..-'|   ||   |")
+        print("'-..-'|_.-;;-._|")
+        print("'-..-'|   ||   |")
+        print("'-..-'|_.-''-._|")
+    }
+}
+
+Solution().solution()
