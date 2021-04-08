63626175d09c0c73a32b7df8f875aebff8376405
zdodev <iver0945@gmail.com>
2021-04-08 21:40:29 +0900

    🧮: 9654번 나부 함대 데이터.

---
+++ Algorithm/Baekjoon/9654-나부-함대-데이터/9654-나부-함대-데이터/main.swift
@@ -0,0 +1,12 @@
+struct Solution {
+    func solution() {
+        print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+        print("N2 Bomber      Heavy Fighter  Limited    21        ")
+        print("J-Type 327     Light Combat   Unlimited  1         ")
+        print("NX Cruiser     Medium Fighter Limited    18        ")
+        print("N1 Starfighter Medium Fighter Unlimited  25        ")
+        print("Royal Cruiser  Light Combat   Limited    4         ")
+    }
+}
+
+Solution().solution()
---
+++ Algorithm/Baekjoon/9654-나부-함대-데이터/README.md
@@ -0,0 +1,15 @@
+###  Baekjoon 9654번 나부 함대 데이터
+
+단순히 다음과 같은 출력을 하는 것이 문제입니다. 띄어쓰기가 포험되어 있습니다.
+
+```swift
+SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+N2 Bomber      Heavy Fighter  Limited    21        
+J-Type 327     Light Combat   Unlimited  1         
+NX Cruiser     Medium Fighter Limited    18        
+N1 Starfighter Medium Fighter Unlimited  25        
+Royal Cruiser  Light Combat   Limited    4         
+```
+
+해당 문자열은 특수기호가 없으므로 그대로 출력해도 문제를 해결할 수 있습니다.
+
