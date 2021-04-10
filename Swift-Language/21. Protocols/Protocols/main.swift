cee5532685630f1e2f08ca84c7f6c802e92b43a0
zdodev <iver0945@gmail.com>
2021-04-10 19:56:00 +0900

    🧾: Class Implementations of protocol initializer requirements.

--- Swift-Language/21. Protocols/Protocols/main.swift
+++ Swift-Language/21. Protocols/Protocols/main.swift
@@ -66,11 +66,98 @@ print(ncc1701.fullName)
 
 // MARK: - Method Requirements
 
+// 타입 메서드 프로토콜 요구사항 정의
+protocol SomeProtocol2 {
+    static func someTypeMethod()
+}
+
+// 메서드 프로토콜 요구사항 정의
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator, SomeProtocol2 {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    
+    // RandomNumberGenerator 프로토콜 요구사항 구현
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy: m))
+        return lastRandom / m
+    }
+    
+    // SomeProtocol2 프로토콜 요구사항 구현
+    static func someTypeMethod() {
+        print("static func someTypeMethod.")
+    }
+}
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+print("And another one: \(generator.random())")
+
 // MARK: - Mutating Method Requirements
+
+// Toggable 프로토콜 요구사항 정의
+protocol Toggable {
+    mutating func toggle()
+}
+
+enum OnOffSwitch: Toggable {
+    case on, off
+    
+    // Toggable 프로토콜 요구사항 구현
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .on
+        case .off:
+            self = .off
+        }
+    }
+}
+
+var lightSwitch = OnOffSwitch.off
+lightSwitch.toggle()
 
 // MARK: - Initializer Requirements
 
+// Initializer 구현을 요구하는 프로토콜 정의
+protocol SomeProtocol3 {
+    init(someParameter: Int)
+}
+
 // MARK: Class Implementations of Protocol Initializer Requirements
+
+// Initializer 구현을 요구하는 프로토콜 채택
+// class에서 채택 시 required 키워드 필수
+class SomeClass1: SomeProtocol3 {
+    required init(someParameter: Int) {
+        print("init")
+    }
+}
+
+// final class에서 채택 시 required 키워드 생략 가능
+final class SomeClass2: SomeProtocol3 {
+    init(someParameter: Int) {
+        print("init")
+    }
+}
+
+// 상위 클래스에서 initializer 정의를 강제하고 있을 때, 하위 클래스에서 다른 initializer를 정의할 경우 상위 클래스에서 요구하는 initializer도 정의해야 한다.
+class SomeSubClass: SomeClass1 {
+    // init()만 있으면 에러
+    init() {
+        super.init(someParameter: 0)
+    }
+    
+    // 상위 클래스에서 정의를 강제하고 있는 initializer
+    required init(someParameter: Int) {
+        fatalError("init(someParameter:) has not been implemented")
+    }
+}
 
 // MARK: Failable Initializer Requirements
 
