786f7868545532bfa48ec0cb6b5b8a984cb91e48
zdodev <iver0945@gmail.com>
2021-04-18 16:05:41 +0900

    ✨: 스택 IteratorProtocol, Sequence 구현.

--- DataStructure/Stack/Stack/main.swift
+++ DataStructure/Stack/Stack/main.swift
@@ -7,3 +7,10 @@ myStack.push(7)
 print(myStack.pop()!)
 print(myStack.pop()!)
 print(myStack.pop()!)
+
+// MARK: - 배열 리터럴 문법 사용
+var array = [4, 5, 6, 7]
+var myStackFromStack = Stack<Int>(array)
+myStackFromStack.push(9)
+myStackFromStack.push(11)
+print(myStackFromStack)
