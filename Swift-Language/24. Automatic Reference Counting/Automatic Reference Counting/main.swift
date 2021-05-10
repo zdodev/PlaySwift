8a0795998a59dbf339e7ceccdfe0f74d7413b09e
zdodev <iver0945@gmail.com>
2021-05-10 22:36:36 +0900

    ✍️: Swift ARC 추가 작성.

--- Swift-Language/24. Automatic Reference Counting/Automatic Reference Counting/main.swift
+++ Swift-Language/24. Automatic Reference Counting/Automatic Reference Counting/main.swift
@@ -147,13 +147,6 @@ unit4A1 = nil
 // deinitializer 호출
 
 //MARK: Unowned References
-/*
- unowned reference도 참조하는 인스턴스의 reference couting을 증가시키지 않습니다. unowned reference는 unowned 키워드를 붙여서 선언합니다.
- 
- unowned reference는 weak reference와 다르게 항상 인스턴스가 존재한다고 생각하고 접근합니다. 그래서 unowned reference는 옵셔널이 될 수 없으며, ARC도 unowned reference 프로퍼티를 nil로 설정하지 않습니다.
- 
- unowned reference는 인스턴스가 절대 해제되지 않을 거라고 생각될 때 사용합니다. 만약 인스턴스가 해제되어 메모리에서 제거된 후 unowned reference 프로퍼티로 접근하면 런타임 에러가 발생합니다.
- */
 
 class Customer {
     let name: String
@@ -213,12 +206,166 @@ let coffee = Coffee(caramel: Caramel())
 
 //MARK: Unowned Optional References
 
+class Department {
+    var name: String
+    // Course strong reference
+    var courses = [Course]()
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Course {
+    var name: String
+    // Department unowned reference
+    unowned var department: Department
+    // Course unowned optional reference
+    // nextCourse에 유요한 인스턴스가 있다는 것은 작성자의 책임이다.
+    unowned var nextCourse: Course?
+    
+    init(name: String, in department: Department) {
+        self.name = name
+        self.department = department
+    }
+}
+
+// reference counting 1
+let department = Department(name: "Horticulture")
+
+// reference counting 1, department는 unowned
+let intro = Course(name: "Survey of Plants", in: department)
+// reference counting 1, department는 unowned
+let intermediate = Course(name: "Growing Common Herbs", in: department)
+// reference counting 1, department는 unowned
+let advanced = Course(name: "Caring for Tropical Plants", in: department)
+intro.nextCourse = intermediate
+intermediate.nextCourse = advanced
+department.courses = [intro, intermediate, advanced]
+
 //MARK: Unowned References and Implicitly Unwrapped Optional Properties
 
+class Country {
+    let name: String
+    // City strong reference
+    // 이니셜라이저에서 self를 사용하기 위해 implicitly unwrapped optional 선언
+    var capitalCity: City!
+    
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+
+class City {
+    let name: String
+    // Country unowned reference
+    unowned let country: Country
+    
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+
+var country = Country(name: "Canada", capitalName: "Ottawa")
+print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+
 //MARK: - Strong Reference Cycles for Closures
+
+class HTMLElement {
+    let name: String
+    let text: String?
+    
+    // 클로저를 strong reference
+    lazy var asHTML: () -> String = {
+        // 클래스 인스턴스 text를 strong reference
+        if let text = self.text {
+            // 클래스 인스턴스 text를 strong reference
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            // 클래스 인스턴스 text를 strong reference
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+}
+print(heading.asHTML())
+
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello world")
+print(paragraph!.asHTML())
+
+paragraph = nil
+// deinitializer가 호출되지 않습니다.
 
 //MARK: - Resolving Strong Reference Cycles for Closures
 
 //MARK: Defining a Capture List
 
+class Bread {
+    let delegate = Caramel()
+
+    lazy var someClosure = {
+        // self, delegate를 캡처하겠습니다.
+        [unowned self, weak delegate = self.delegate]
+        (index: Int, stringToProcess: String) -> String in
+        // weak 옵셔널이므로 언래핑하여 사용
+        delegate!.flavor
+    }
+    
+    lazy var someClosure1 = {
+        // self, delegate를 캡처하겠습니다.
+        [unowned self, unowned delegate = self.delegate]
+        (index: Int, stringToProcess: String) -> String in
+        // unowned 이므로 바로 사용
+        delegate.flavor
+    }
+}
+
 //MARK: Weak and Unowned References
+
+class HTMLElement1 {
+    let name: String
+    let text: String?
+    
+    // 클로저를 strong reference
+    lazy var asHTML: () -> String = {
+        [unowned self] in
+        // 클래스 인스턴스 text를 unowned reference
+        if let text = self.text {
+            // 클래스 인스턴스 text를 unowned reference
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            // 클래스 인스턴스 text를 unowned reference
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var paragraph1: HTMLElement1? = HTMLElement1(name: "p", text: "hello world")
+print(paragraph1!.asHTML())
+paragraph1 = nil
+// p is being deinitialized
