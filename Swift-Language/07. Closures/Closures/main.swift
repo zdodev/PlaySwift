0eedce6f976a440227160289a7cc1583a2def78f
zdodev <iver0945@gmail.com>
2021-05-24 11:10:30 +0900

    🙇‍♂️: 클로저 정리.

--- Swift-Language/07. Closures/Closures/main.swift
+++ Swift-Language/07. Closures/Closures/main.swift
@@ -15,32 +15,20 @@ print(reversedNames)
 
 // MARK: Closure Expression Syntax
 
-// sorted(by:) 메서드의 파라미터 타입: (String, String) -> Bool
-
-/*
- 클로저 정의
- { (s1: String, s2: String) -> Bool in
-     return s1 > s2
- }
- */
 reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
     return s1 > s2
 })
 
-
 // MARK: Inferring Type from Context
 
-// sorted(by:) 파라미터는 (String, String) -> Bool 타입을 받으므로 클로저에서 파라미터 타입과 리턴 타입을 생략해도 타입을 추론할 수 있다.
-
-/*
- 파라미터 타입과 리턴 타입을 생략한 클로저
- { s1, s2 in
-     return s1 > s2
- }
- */
 reversedNames = names.sorted(by: { s1, s2 in
     return s1 > s2
 })
+
+var inferringType: (Double, Double) -> Bool
+inferringType = { height, row in
+    return height < row
+}
 
 // MARK: Implicit Returns from Single-Expression Closures
 
@@ -48,26 +36,18 @@ reversedNames = names.sorted(by: { s1, s2 in
     s1 > s2
 })
 
+var implicitReturn: (Double, Double) -> Bool
+implicitReturn = { height, row in
+    height < row
+}
+
 // MARK: Shorthand Argument Names
-
-// sorted(by:) 파라미터는 (String, String) -> Bool 타입을 받으므로 인자명을 정의하지 않고 $0, $1와 같이 단축 인자명을 사용할 수 있으며 in 키워드를 생략할 수 있다.
-
-/*
- 파라미터명과 in 키워드를 생략한 클로저
- {
-     $0 > $1
- }
- */
 
 reversedNames = names.sorted(by: {
     $0 > $1
 })
 
 // MARK: Operator Methods
-
-// sorted(by:) 파라미터는 (String, String) -> Bool 타입을 받는다. 그리고 (String, String) -> Bool 이 구현된 연산자가 있을 경우 연산자만을 사용하여 클로저를 표현할 수 있다.
-
-// static func > (lhs: String, rhs: String) -> Bool
 
 reversedNames = names.sorted(by: >)
 
