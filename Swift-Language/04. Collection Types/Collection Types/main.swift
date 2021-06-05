4304d3d34687bef7ed7fa2c2122073604282df7b
zdodev <iver0945@gmail.com>
2021-06-05 12:57:32 +0900

    🙇‍♂️: Swift Collection Types 목차 정리.

--- Swift-Language/04. Collection Types/Collection Types/main.swift
+++ Swift-Language/04. Collection Types/Collection Types/main.swift
@@ -1,16 +1,12 @@
-/// Collection Types
+//MARK: - Collection Types
 
-/// Multability of Collections
+//MARK: - Multability of Collections
 
-/// Arrays
+//MARK: - Arrays
 
-/// Array Type Shorthand Syntax
-/*
- struct Array<Element>
- [Element]
- */
+//MARK: Array Type Shorthand Syntax
 
-/// Creating an Empty Array
+//MARK: Creating an Empty Array
 
 var someInts = [Int]()
 someInts.append(3)
@@ -18,24 +14,24 @@ print("someInts = \(someInts)")
 someInts = []
 print("someInts = \(someInts)")
 
-/// Creating an Array with a Default Value
+//MARK: Creating an Array with a Default Value
 
 var threeDoubles = Array(repeating: 0.0, count: 3)
 print("threeDoubles = \(threeDoubles)")
 
-/// Creating an Array by Adding Two Arrays Together
+//MARK: Creating an Array by Adding Two Arrays Together
 
 var antoherThreeDoubles = Array(repeating: 2.5, count: 3)
 var sixDoubles = threeDoubles + antoherThreeDoubles
 print("sixDoubles = \(sixDoubles)")
 
-/// Creating an Array with an Array Literal
+//MARK: Creating an Array with an Array Literal
 
 var shoppingList: [String] = ["Eggs", "Milk"]
 print("shoppingList = \(shoppingList)")
 var shoppingList1 = ["Eggs", "Milk"]
 
-/// Accessing and Modifying an Array
+//MARK: Accessing and Modifying an Array
 
 print("The shopping list contains \(shoppingList.count) items.")
 
@@ -74,7 +70,7 @@ print("firstItem = \(firstItem)")
 let apples = shoppingList.removeLast()
 print("apples = \(apples)")
 
-/// Iterating Over an Array
+//MARK: Iterating Over an Array
 
 for item in shoppingList {
     print("iteraing \(item)...")
@@ -83,16 +79,13 @@ shoppingList.map {
     print($0)
 }
 
-/// Sets
+//MARK: - Sets
 
-/// Hash Values for Set Types
+//MARK: Hash Values for Set Types
 
-/// Set Type Syntax
-/*
- Set<Element>
- */
+//MARK: Set Type Syntax
 
-/// Creating and Initializing an Empty Set
+//MARK: Creating and Initializing an Empty Set
 
 var letters = Set<Character>()
 print("letters is of type Set<Character> with \(letters.count) items.")
@@ -101,14 +94,14 @@ print("letters = \(letters)")
 letters = []
 print("letters = \(letters)")
 
-/// Creating a Set wit an Array Literal
+//MARK: Creating a Set wit an Array Literal
 
 var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
 print("favoriteGenres: \(favoriteGenres)")
 var favoriteGenres1: Set = ["Rock", "Classical", "Hip hop"]
 print("favoriteGenres1: \(favoriteGenres1)")
 
-/// Accessing and Modifying a Set
+//MARK: Accessing and Modifying a Set
 
 print("I have \(favoriteGenres.count) favorite music genres.")
 
@@ -132,7 +125,7 @@ if favoriteGenres.contains("Funk") {
     print("It's too funky in here.")
 }
 
-/// Iterating Over a Set
+//MARK: Iterating Over a Set
 
 for genre in favoriteGenres {
     print(genre)
@@ -142,9 +135,9 @@ for genre in favoriteGenres.sorted() {
     print(genre)
 }
 
-/// Performing Set Operations
+//MARK: Performing Set Operations
 
-/// Fundamental Set Operations
+//MARK: Fundamental Set Operations
 
 let oddDigits: Set<Int> = {
     var set = Set<Int>()
@@ -176,7 +169,7 @@ print("subtracting = \(subtracting)")
 let symmetricDifference = oddDigits.symmetricDifference(evenDigits).sorted()
 print("symmetricDifference = \(symmetricDifference)")
 
-/// Set Membership and Equality
+//MARK: Set Membership and Equality
 
 let houseAnimals: Set = ["🐶", "🐱"]
 let farmAnimals: Set = ["🐮", "🐔", "🐑", "🐶", "🐱"]
@@ -191,22 +184,18 @@ print("superset = \(superset)")
 let disjoint = farmAnimals.isDisjoint(with: cityAnimals)
 print("disjoint = \(disjoint)")
 
-/// Dictionaries
+//MARK: - Dictionaries
 
-/// Dictionary Type Shorhand Syntax
-/*
- Dictionary<Key, Value>
- [Key: Value]
- */
+//MARK: Creating an Empty Dictionary
 
-/// Creating an Empty Dictionary
+var dictionary = Dictionary<Int, String>()
 
 var namesOfIntegers = [Int: String]()
 namesOfIntegers[16] = "sixteen"
 namesOfIntegers = [:]
 print("namesOfIntegers = \(namesOfIntegers)")
 
-/// Creating a Dictionary with a Dictionary Literal
+//MARK: Creating a Dictionary with a Dictionary Literal
 
 var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
 print("airports = \(airports)")
@@ -214,7 +203,7 @@ print("airports = \(airports)")
 var airports1 = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
 print("airpost1 = \(airports1)")
 
-/// Accessing and Modifying a Dictionary
+//MARK: Accessing and Modifying a Dictionary
 
 print("The airports dictionary contains \(airports.count) items.")
 
@@ -250,7 +239,7 @@ if let removedValue = airports.removeValue(forKey: "DUB") {
     print("The airports dictionary does not contain a value for DUB.")
 }
 
-/// Iterating Over a Dictionary
+//MARK: Iterating Over a Dictionary
 
 for (airportCode, airportName) in airports {
     print("\(airportCode): \(airportName)")
---
+++ Swift-Language/04. Collection Types/README.md
@@ -0,0 +1,55 @@
+# Collection Types
+
+## Mutability of Collections
+
+## Arrays
+
+### Array Type Shorthand Syntax
+
+### Creating an Empty Array
+
+### Creating an Array with a Default Value
+
+### Creating an Array by Adding Two Arrays Together
+
+### Creating an Array with an Array Literal
+
+### Accessing and Modifying an Array
+
+### Iterating Over an Array
+
+## Sets
+
+### Hash Values for Set Types
+
+### Set Type Syntax
+
+### Creating and Initializing an Empty Set
+
+### Creating a Set with an Array Literal
+
+### Accessing and Modifying a Set
+
+### Iterating Over a Set
+
+## Performing Set Operations
+
+### Fundamental Set Operations
+
+### Set Membership and Equality
+
+## Dictionaries
+
+딕셔너리는 키 그리고 키과 연관된 값으로 이루어진 타입입니다. 키는 유일하며 값과 연관되어 있습니다. 딕셔너리는 순서가 없습니다. Foundation 프레임워크의 NSDictionary 타입과 연동되어 있습니다.
+
+### Dictionary Type Shorthand Syntax
+
+딕셔너리는 Dictionary<Key, Value>로 선언할 수 있습니다. 딕셔너리의 키는 유일해야 하므로 Hashable 프로토콜을 준수하는 타입만 딕셔너리의 키가 될 수 있습니다.
+
+### Creating an Empty Dictionary
+
+### Creaing a Dictionary with a Dictionary Literal
+
+### Accessing and Modifying a Dictionary
+
+### Iterating Over a Dictionary
