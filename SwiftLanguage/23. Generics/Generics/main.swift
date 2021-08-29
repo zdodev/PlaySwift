d0f0793ffd1f27cd1a092b6ba7729fbfc9565a9f
zdodev <iver0945@gmail.com>
2021-08-30 00:39:14 +0900

    📝: Swift Generics.

--- SwiftLanguage/23. Generics/Generics/main.swift
+++ SwiftLanguage/23. Generics/Generics/main.swift
@@ -2,6 +2,29 @@
 
 // MARK: - The Problem that Generics Solve
 
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+func swapTwoStrings(_ a: inout String, _ b: inout String) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
 // MARK: - Generic Functions
 
 // MARK: - Type Parameters
