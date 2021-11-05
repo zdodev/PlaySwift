bec6c705192d0ed7748d4b255f2c37493dacd91f
zdodev <iver0945@gmail.com>
2021-11-05 17:25:01 +0900

    ✨: Coffee 인스턴스 deinit 예제 작성

--- SwiftLanguage/15. Deinitialization/Deinitialization/main.swift
+++ SwiftLanguage/15. Deinitialization/Deinitialization/main.swift
@@ -3,3 +3,31 @@
 // MARK: - How Deinitialization Works
 
 // MARK: - Deinitializers in Action
+
+class Cafe {
+    var coffee: Coffee?
+    
+    init(coffee: Coffee) {
+        self.coffee = coffee
+    }
+    
+    func deinitCoffee() {
+        coffee = nil
+    }
+    
+    func make() {
+        let _ = Coffee()
+    }
+}
+
+class Coffee {
+    deinit {
+        print("\(Self.self) deinit")
+    }
+}
+
+let cafe = Cafe(coffee: Coffee())
+// Coffee 인스턴스 해제 1
+cafe.deinitCoffee()
+// Coffee 인스턴스 해제 2
+cafe.make()
